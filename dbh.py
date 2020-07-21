34767f2938deb77ad6656d261fff1c7ad2c8f0f5
Gavin Quirk <gavinquirk@gmail.com>
2020-07-21 16:34:00 -0700

    use replace raw strings using os.path.join, open game

--- dbh.py
+++ dbh.py
@@ -1,18 +1,24 @@
 import os
+import subprocess
 
 # Overlay files to be removed
-overlay_files = [r'\SteamOverlayVulkanLayer.json',
-                 r'\SteamOverlayVulkanLayer64.json']
+overlay_files = ['SteamOverlayVulkanLayer.json',
+                 'SteamOverlayVulkanLayer64.json']
 
 # Path of Steam installation
 steam_path = r'F:\Program Files (x86)\Steam'
 
 # Loop over list of overlay files and remove them from Steam directory
 for overlay_file in overlay_files:
-    file_path = steam_path + overlay_file
+    file_path = os.path.join(steam_path, overlay_file)
+    print(file_path)
 
     if os.path.exists(file_path):
         os.remove(file_path)
         print("File found and removed")
     else:
         print("The file does not exist")
+
+# Launch game
+subprocess.Popen(
+    [r"C:\SteamLibrary\steamapps\common\Detroit Become Human\DetroitBecomeHuman.exe"])
